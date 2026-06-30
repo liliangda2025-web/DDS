b6fc8eab12f94da48f918a91a335c167d931ee24
li-liangda <liliangda5450@qq.com>
2026-06-30 09:56:10 +0800

    feat：初始化springboot项目

---
+++ src/main/java/com/example/dqcadirsystem/DqcadirSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.dqcadirsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DqcadirSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DqcadirSystemApplication.class, args);
+    }
+
+}
